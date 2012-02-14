b1f5dd3ba20299c1194fb46c3fe5e69d88d526fb
Vincent Cantin <vincent.cantin@le-moulin-studio.com>
2012-02-15 02:49:59 +0800

    Added tests for other block ciphers.

--- platform/src/test/java/com/lemoulinstudio/forest/platform/CryptographyTest.java
+++ platform/src/test/java/com/lemoulinstudio/forest/platform/CryptographyTest.java
@@ -6,7 +6,13 @@ import java.security.KeyPair;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.util.Arrays;
+import org.bouncycastle.crypto.BlockCipher;
 import org.bouncycastle.crypto.engines.AESEngine;
+import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.engines.AESLightEngine;
+import org.bouncycastle.crypto.engines.CAST5Engine;
+import org.bouncycastle.crypto.engines.CAST6Engine;
 import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -82,17 +88,16 @@ public class CryptographyTest {
     //KeyExporter.exportPublicKey(importedAlicePublicKey, System.out, true);
   }
   
-  @Test
-  public void testAesEncryptionDecryption() throws Exception {
+  private void testCipherEncryptionDecryption(BlockCipher cipherEngine, int keySizeInBits) throws Exception {
     // Let's define a data.
     byte[] dataToEncrypt = famousQuote.getBytes();
     
     // Let's choose a random encryption/decryption key.
-    byte[] secretKey = new byte[256 / 8];
+    byte[] secretKey = new byte[keySizeInBits / 8];
     new SecureRandom().nextBytes(secretKey);
     
     // Prepare the objects to encrypt the data.
-    PaddedBufferedBlockCipher encryptCipher = new PaddedBufferedBlockCipher(new AESEngine());
+    PaddedBufferedBlockCipher encryptCipher = new PaddedBufferedBlockCipher(cipherEngine);
     encryptCipher.init(true, new KeyParameter(secretKey));
     byte[] encryptedData = new byte[dataToEncrypt.length + encryptCipher.getBlockSize() * 2];
     
@@ -101,13 +106,16 @@ public class CryptographyTest {
     nbEncryptedBytes += encryptCipher.doFinal(encryptedData, nbEncryptedBytes);
     
     // Prepare the objects to decrypt the data.
-    PaddedBufferedBlockCipher decryptCipher = new PaddedBufferedBlockCipher(new AESEngine());
+    PaddedBufferedBlockCipher decryptCipher = new PaddedBufferedBlockCipher(cipherEngine);
     decryptCipher.init(false, new KeyParameter(secretKey));
     byte[] decryptedData = new byte[dataToEncrypt.length];
     
     // Decrypt the data.
     int nbDecryptedBytes = decryptCipher.processBytes(encryptedData, 0, nbEncryptedBytes, decryptedData, 0);
     nbDecryptedBytes += decryptCipher.doFinal(decryptedData, nbDecryptedBytes);
+    
+    assertFalse("The data should not be the same after encryption.",
+            Arrays.equals(dataToEncrypt, Arrays.copyOfRange(encryptedData, 0, dataToEncrypt.length)));
     
     assertEquals("The decrypted data should have the same size that the original one.",
             dataToEncrypt.length,
@@ -116,6 +124,19 @@ public class CryptographyTest {
     assertArrayEquals("The data should be the same before encryption and after decryption.",
             dataToEncrypt,
             decryptedData);
+    
+    // Prints the data and its encrypted form.
+    //System.out.println("dataToEncrypt = " + Arrays.toString(dataToEncrypt));
+    //System.out.println("encryptedData = " + Arrays.toString(encryptedData));
+  }
+  
+  @Test
+  public void testCiphersEncryptionDecryption() throws Exception {
+    testCipherEncryptionDecryption(new AESEngine(),      256); // keysizes in bits: 128, 192, 256.
+    testCipherEncryptionDecryption(new AESFastEngine(),  256);
+    testCipherEncryptionDecryption(new AESLightEngine(), 256);
+    testCipherEncryptionDecryption(new CAST5Engine(),    128); // keysizes in bits: 40, 64, 80, 128.
+    testCipherEncryptionDecryption(new CAST6Engine(),    256); // keysizes in bits: 128, 160, 192, 224, 256.
   }
   
 }
